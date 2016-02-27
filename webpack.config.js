f593f340a6dd296809a9ea37e120a4d50e69a4c9
Jonny Andreola <jonnyandreola@gmail.com>
2016-02-27 12:39:46 -0500

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: './',
+  entry: './index.js',
+  output: {
+    path: './dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
