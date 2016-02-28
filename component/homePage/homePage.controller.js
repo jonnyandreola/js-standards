fc2da829bc01609abe892b323e680a2d0bb89d0f
Jonny Andreola <jonnyandreola@gmail.com>
2016-02-28 00:12:43 -0500

    stuff

--- component/homePage/homePage.controller.js
+++ component/homePage/homePage.controller.js
@@ -1,7 +1,10 @@
 import User from '../../model/User';
 
 export default function homePageController () {
-
+	/**
+	 * Naming this 'ctrl' make easier to copy and paste
+	 * between controller and view
+	 */
 	const ctrl = {};
 
 	ctrl.user = new User({name: 'John Doe'});
