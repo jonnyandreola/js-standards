533bd60cea21f358e0bc9b77234009de55c7eadb
Jonny Andreola <jandreola@klick.com>
2016-03-01 17:23:58 -0500

    added test file

---
+++ template/component/COMPONENT_NAME.test.js
@@ -0,0 +1,17 @@
+;(function (){
+    'use strict';
+
+    var expect = require('chai').expect;
+    var fs = require('fs');
+    var vm = require('vm');
+    var /*COMPONENT_NAME*/ = require('.//*COMPONENT_NAME*/')
+
+    describe('/*COMPONENT_NAME*/', function() {
+        describe('MethodName', function () {
+            it('method description', function () {
+               expect(/*COMPONENT_NAME*/.methodName().to.equal();
+            });
+        });
+    });
+
+}());
